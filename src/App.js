ee1574b1f711eb4fb116eec6bfe2665996ee435c
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-15 20:22:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 // Import Components
 import { Header } from './Components/Header';
+import { Balance } from './Components/Balance';
 
 function App() {
   return (
     <div>
       <Header/>
       <div className="container">
-        <h5>Balance</h5>
+        <Balance />
       </div>
     </div>
   );
