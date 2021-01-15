595263fa3e28c92433b0339bf17159eb1911523e
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-15 20:45:00 +0500

    Update AddTransaction.jsx

--- src/Components/AddTransaction.jsx
+++ src/Components/AddTransaction.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const AddTransaction = () => {
+
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
     return (
         <div>
             <h3>Add new transaction</h3>
       <form >
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e)=> setText(e.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
