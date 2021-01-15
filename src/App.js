60ff89f1b025778d4f28b6417821ec8756e1b7f2
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-15 20:32:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import { Header } from './Components/Header';
 import { Balance } from './Components/Balance';
 import { IncomeExpenses } from './Components/IncomeExpenses';
+import { TransactionList } from './Components/TransactionList';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
