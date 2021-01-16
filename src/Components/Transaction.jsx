34b68976d01097469ac1a6d94fc9de7293226515
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-16 14:16:48 +0500

    Update Transaction.jsx

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalContext} from '../Context/GlobalContext';
 
 export const Transaction = ({transaction}) => {
+
+  const {deleteTransactions} = useContext(GlobalContext);
 
   const sign = transaction.amount < 0 ? '-' : '+';
 
     return (
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-          {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+          {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn" onClick={()=> deleteTransactions(transaction.id)}>x</button>
         </li>
     )
 }
