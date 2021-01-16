9ea0c85a39fa7981ae4109cb872e4d1c7abe113e
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-16 13:15:34 +0500

    Update Balance.jsx

--- src/Components/Balance.jsx
+++ src/Components/Balance.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {GlobalContext} from '../Context/GlobalContext';
 
 export const Balance = () => {
+
+    const {transactions} = useContext(GlobalContext);
+
     return (
         <div>
             <h4>Your Balance</h4>
