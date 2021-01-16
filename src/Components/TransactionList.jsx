5ed00b3e061170f1d204926cc650e2ccfbe1ccfe
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-16 12:54:38 +0500

    Update TransactionList.jsx

--- src/Components/TransactionList.jsx
+++ src/Components/TransactionList.jsx
@@ -3,15 +3,18 @@ import {GlobalContext} from '../Context/GlobalContext';
 
 export const TransactionList = () => {
 
-  const context = useContext(GlobalContext);
+  const {transactions} = useContext(GlobalContext);
+
 
     return (
         <>
             <h3>History</h3>
       <ul id="list" className="list">
-         <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
+        {transactions.map(transaction => (
+          <li className="minus">
+          {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
         </li>
+        ))}
       </ul>
         </>
     );
