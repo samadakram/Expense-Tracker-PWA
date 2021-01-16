be57086b71040cd293dfade1bcb93bd45a403f63
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-16 12:57:16 +0500

    Update TransactionList.jsx

--- src/Components/TransactionList.jsx
+++ src/Components/TransactionList.jsx
@@ -10,11 +10,7 @@ export const TransactionList = () => {
         <>
             <h3>History</h3>
       <ul id="list" className="list">
-        {transactions.map(transaction => (
-          <li className="minus">
-          {transaction.text} <span>{transaction.amount}</span><button className="delete-btn">x</button>
-        </li>
-        ))}
+        {transactions.map(transaction => ())}
       </ul>
         </>
     );
