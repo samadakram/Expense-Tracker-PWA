bf7cac31380af02773d335da7f6d9b8d0ac79697
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-16 13:00:44 +0500

    Delete Transaction.js

--- src/Components/Transaction.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Transaction = () => {
-    return (
-        <li className="minus">
-          {transaction.text} <span>{transaction.amount}</span><button className="delete-btn">x</button>
-        </li>
-    )
-}
