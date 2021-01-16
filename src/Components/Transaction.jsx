937282c87249eb25bfa8338b39b59b5d0d5f183b
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-16 13:02:30 +0500

    Update Transaction.jsx

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Transaction = () => {
+export const Transaction = ({transaction}) => {
     return (
         <li className="minus">
           {transaction.text} <span>{transaction.amount}</span><button className="delete-btn">x</button>
