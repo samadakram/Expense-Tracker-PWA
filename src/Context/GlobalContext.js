3580842f059d7c3d1a7e8b94f9261e46b136bde6
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-16 13:32:23 +0500

    Update GlobalContext.js

--- src/Context/GlobalContext.js
+++ src/Context/GlobalContext.js
@@ -19,6 +19,15 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    // Actions
+
+    function deleteTransactions (id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
     return(
         <GlobalContext.Provider value={{
             transactions: state.transactions
