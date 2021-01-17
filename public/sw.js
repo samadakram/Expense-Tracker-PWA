635ddbf48b1f009ed17831ff4bbcc346ffaca1aa
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-18 01:09:35 +0500

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,51 @@
+let version = "v3";
+
+//Cache Files
+let cacheFiles = [
+  "/static/js/bundle.js",
+  "/static/js/0.chunk.js",
+  "/static/js/main.chunk.js",
+  "/logo192.png",
+  "/",
+  "/index.html",
+];
+
+// Install Service Woker
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(version).then((cache) => {
+      console.log("cacheFiles", cacheFiles);
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", function (e) {
+  console.log("[ServiceWorker] Activate");
+});
+
+const options = {
+  ignoreSearch: true,
+  ignoreMethod: true,
+  ignoreVary: true,
+};
+// Fetch Service Worker
+self.addEventListener("fetch", (event) => {
+  if (!navigator.onLine) {
+    event.respondWith(
+      caches
+        .match(event.request, options)
+        .then((response) => {
+          if (response) {
+            // console.log(response);
+            return response || fetch.response;
+          }
+        })
+        .catch((err) => {
+          console.log("err", err);
+        })
+    );
+  }
+});
