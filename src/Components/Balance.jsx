188c53a72981d37788a80bfd5caedd3214cd3499
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-17 13:32:05 +0500

    Update Balance.jsx

--- src/Components/Balance.jsx
+++ src/Components/Balance.jsx
@@ -12,7 +12,7 @@ export const Balance = () => {
     return (
         <div>
             <h4>Your Balance</h4>
-            <h1 id="balance">${total}</h1>
+            <h1>${total}</h1>
         </div>
     );
 }
