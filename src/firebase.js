bc93cd74d58f7e0573784640fa7157133d03c405
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-18 15:03:29 +0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDUCizXMZZRRx00gwO1kzzSvJS4neBd9v8",
+    authDomain: "expense-tracker-samad.firebaseapp.com",
+    projectId: "expense-tracker-samad",
+    storageBucket: "expense-tracker-samad.appspot.com",
+    messagingSenderId: "276398541172",
+    appId: "1:276398541172:web:64b3e606d37acd28300421"
+};
+
+firebase.initializeApp(config);
